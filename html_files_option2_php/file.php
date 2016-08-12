772dc2c181fc7a8fc5dced84cec723f167ab11b1
root <you@example.com>
2016-08-12 02:52:44 +0000

    see the  html_files_option2_php folder for the working python script - command log in wiki page

---
+++ html_files_option2_php/file.php
@@ -0,0 +1,17 @@
+some pile of text
+or other
+<!-- Footer part at bottom of page-->
+<div id="footer">
+   <div class="row col-md-2 col-md-offset-5">
+
+    <?php
+	$year = date("Y");
+	echo "<p class='text-muted'>© $year. Core Team</p>";
+	?>
+  </div>
+
+    <div id="downloadlinks">
+    <!-- downloadlinks go here-->
+    </div>
+</div>
+and more maybe.
